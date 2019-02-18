e46aec0dca5a07ab2dca3bcae3a22fd2ee8c0cba
Huanggan <704484570@qq.com>
2019-02-18 17:43:24 +0800

    hg

--- src/main/java/cn/hg/task/task/mapper/TaskMapper.java
+++ src/main/java/cn/hg/task/task/mapper/TaskMapper.java
@@ -1,9 +1,10 @@
 package cn.hg.task.task.mapper;
 
-import cn.hg.task.task.entity.Group;
 import cn.hg.task.task.entity.Task;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 public interface TaskMapper extends BaseMapper<Task> {
+
+
 
 }
--- src/main/java/cn/hg/task/user/controller/UserController.java
+++ src/main/java/cn/hg/task/user/controller/UserController.java
@@ -1,10 +1,21 @@
 package cn.hg.task.user.controller;
 
 import cn.hg.task.common.BaseController;
+import cn.hg.task.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 public class UserController  extends BaseController {
+
+    @Autowired
+    UserMapper userMapper;
+
+
+
+
 
 
 }
