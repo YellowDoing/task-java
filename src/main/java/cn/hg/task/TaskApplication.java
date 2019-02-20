887f4773e13704e6ba9b9ed1e7c522885db2d7e9
Huanggan <704484570@qq.com>
2019-02-20 15:41:23 +0800

    hg

--- src/main/java/cn/hg/task/TaskApplication.java
+++ src/main/java/cn/hg/task/TaskApplication.java
@@ -1,8 +1,10 @@
 package cn.hg.task;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("cn.hg.task.test*")
 @SpringBootApplication
 public class TaskApplication {
 
